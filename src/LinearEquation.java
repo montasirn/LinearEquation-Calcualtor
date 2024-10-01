3b8efa44b3ac36cb99442071a46e9b3ac57986b6
montasirn <montasirn@nycstudents.net>
2024-10-01 10:50:24 -0400

    LinearEquation.java updated, added methods for slope, y-int, equation, and thirdPoint

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -2,12 +2,24 @@ public class LinearEquation {
     private int x;
     private int y;
     private double slope;
+    private double yIntercept;
 
     public LinearEquation(int x1, int y1) {
-        int x = x1;
-        int y = y1;
+        x = x1;
+        y = y1;
     }
-    public double slope(int x1, int x2, int y1, int y2){
-        return ((double) x2 - x1) / ((double) y2 - y1);
+    public double slope(int x2, int y2){
+        slope = ((double) x2 - x) / ((double) y2 - x);
+        return slope;
+    }
+    public double yIntercept(){
+        yIntercept = y - (x * slope);
+        return yIntercept;
+    }
+    public String equation(){
+        return "" + y + "= " + slope + "x" + " + " + yIntercept;
+    }
+    public String thirdPoint(){
+        return "hi";
     }
 }
