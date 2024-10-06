edd0e3adb448fcfc768ec1a40792abe2aa285919
montasirn <montasirn@nycstudents.net>
2024-10-06 16:08:24 -0400

    Updated Design Document to better fit how the program has been written i.e added new instance variables added methods and changed how some methods were planned to be executed
    Updated the EquationRunner.java class to get welcome the user and get their input as a string before extracting the x and y coordinates, the correct results are printed then the program prompts the user for a third point before returning the correct coordinates for the inputted x value.
    
    Updated the LinearEquation class to format the slope as a fraction correctly as well as print the correct third point. The equation method was fixed to display the correct equation based on the conditions.

--- src/EquationRunner.java
+++ src/EquationRunner.java
@@ -2,8 +2,36 @@ import java.util.Scanner;
 
 public class EquationRunner {
     public static void main(String[] args) {
+        // set up scanner for user input
         Scanner s = new Scanner(System.in);
-        LinearEquation test = new LinearEquation(2,4,4,8);
+
+        //Welcoming user and prompt them for their coordinate pairs
+        System.out.println("Welcome to the best Linear Equation Calculator of 2024");
+        System.out.println("To begin, enter your first pair of points (in (x,y) format): ");
+        String pair1 = s.nextLine();
+        System.out.println("Now enter the second pair of points (in (x,y) format*): ");
+        String pair2 = s.nextLine();
+
+        //get out the numbers from the string inputs
+        String xPair1 = pair1.substring(1, pair1.indexOf(",")); // Extract x from pair1
+        String yPair1 = pair1.substring(pair1.indexOf(" ") + 1, pair1.indexOf(")")); // Extract y from pair1
+        String xPair2 = pair2.substring(1, pair2.indexOf(",")); // Extract x from pair2
+        String yPair2 = pair2.substring(pair2.indexOf(" ") + 1, pair2.indexOf(")")); // Extract y from pair2
+
+
+        //convert the numbers from Strings to integers
+        int xPair1conv = Integer.parseInt(xPair1); //get the first x coordinate
+        int xPair2conv = Integer.parseInt(xPair2); //get the second x coordinate
+        int yPair1conv = Integer.parseInt(yPair1); //get the first y coordinate
+        int yPair2conv = Integer.parseInt(yPair2); //get the second y coordinate
+
+        LinearEquation test = new LinearEquation(xPair1conv,yPair1conv,xPair2conv,yPair2conv);
         System.out.println(test.toString());
+
+        //prompt user for a third point
+        System.out.println("Enter a x value to see the y value at that point");
+        int thirdPoint = s.nextInt();
+        LinearEquation thirdPointY = new LinearEquation(xPair1conv,yPair1conv,xPair2conv,yPair2conv);
+        thirdPointY.thirdPointLQ(thirdPoint);
     }
 }
--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -5,51 +5,108 @@ public class LinearEquation {
     private int y2;
     private double slope;
     private double yIntercept;
+    private String slopeFraction;
 
+    // Constructor for the class
     public LinearEquation(int x1, int y1, int x2, int y2) {
         this.x1 = x1;
         this.y1 = y1;
         this.x2 = x2;
         this.y2 = y2;
+        calculateSlopeAndYIntercept();
     }
-    public double slope(){
-        slope = (double) (x2 - x1) / (y2 - y1);
-        return slope;
-    }
-    public double yIntercept(){
-        yIntercept = y1 - (x1 * slope);
-        return yIntercept;
-    }
-    public int gcd(){
-        int numerator = (x2 - x1);
-        int denominator = (y2 - y1);
-        return numerator % denominator;
 
+    public void thirdPointLQ(int a) {
+        // Calculate the y value using the formula y = mx + b
+        double y = slope * a + yIntercept;
+
+        // Print the coordinate pair
+        String s = "(" + a + ", " + y + ")";
+        System.out.println(s);
     }
-    public String slopeString(){
-        int numerator = (x2 - x1);
-        int denominator = (y2 - y1);
-        if(numerator == denominator){
-            return slope() + "";
-        } else {
-            return numerator/gcd() + "/" + denominator/gcd();
+
+    // Method to calculate slope
+    private void calculateSlopeAndYIntercept() {
+        slope = (double) (y2 - y1) / (x2 - x1);  // Fixing the slope calculation
+        yIntercept = y1 - slope * x1;
+    }
+
+
+    // Method to calculate the greatest common divisor (GCD)
+    private int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
         }
+        return gcd(b, a % b);
     }
-    public String equation(){
-        return "y" + "= " + slopeString() + "x" + " + " + yIntercept();
+
+    // Method to return slope as a simplified fraction
+    public String slopeString() {
+        int numerator = (y2 - y1);
+        int denominator = (x2 - x1);
+
+        // If slope is 0, return "0"
+        if (numerator == 0) {
+            slopeFraction = "0";
+            return slopeFraction;
+        }
+
+        // Find the greatest common divisor to reduce the fraction
+        int gcdValue = gcd(numerator, denominator);
+        numerator /= gcdValue;
+        denominator /= gcdValue;
+
+        // If both numerator and denominator are negative -> make them positive
+        if (numerator < 0 && denominator < 0) {
+            numerator = -numerator;
+            denominator = -denominator;
+        }
+
+        // If the denominator is negative, move the negative sign to the numerator
+        if (denominator < 0) {
+            numerator = -numerator;
+            denominator = -denominator;
+        }
+
+        // Handle cases where denominator is 1 or -1
+        if (denominator == 1) {
+            slopeFraction = numerator == 1 ? "" : String.valueOf(numerator); // No need for '1x'
+        } else if (denominator == -1) {
+            slopeFraction = numerator == -1 ? "-" : String.valueOf(-numerator); // No need for '-1x'
+        } else {
+            slopeFraction = numerator + "/" + denominator; // Normal fraction
+        }
+
+        return slopeFraction;
     }
-    public String thirdPoint(int x){
-        return ("(" + x + ", " + x * slope + yIntercept + ")");
+
+    // Method to return the equation of the line
+    public String equation() {
+        // Special case when slope is zero
+        if (slope == 0) {
+            return "y = " + yIntercept; // Just return y = b
+        }
+
+        // General case when slope is non-zero
+        String slopeStr = slopeString();
+        String yInterceptStr = yIntercept == 0 ? "" : (yIntercept > 0 ? " + " + yIntercept : " - " + Math.abs(yIntercept)); // Makes sure the right symbol is used when the y-int is negative; so it doesn't return as + -
+
+        return "y = " + slopeStr + "x" + yInterceptStr;
     }
-    public double disatnce(){
+
+    // Method to calculate the distance between two points
+    public double distance(){
+
         return Math.sqrt((Math.pow((x2 - x1),2) + Math.pow((y2 - y1),2)));
     }
+
+
     public String toString(){
         return "First pair: (" + x1 + ", " + y1 + ") \n" +
                 "Second pair: (" + x2 + ", " + y2 + ") \n" +
                 "Equation: " + equation() + "\n" +
-                "Slope: " + slope() + "\n" +
-                "Y-Intercept: " + yIntercept() + "\n" +
-                "Distance between points: " + disatnce() + "\n";
+                "Slope: " + slopeString() + "\n" +
+                "Y-Intercept: " + yIntercept + "\n" +
+                "Distance between points: " + distance() + "\n";
     }
 }
